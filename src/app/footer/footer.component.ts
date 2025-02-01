f847cf4547a61801517c7de01bc43e9e47a135d1
Sudarshan P <sudarshan.pal98@gmail.com>
2025-02-01 14:08:34 +0530

    added notification number on cart

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { Product } from '../product.interface';
+import { CartService } from '../cart/cart.service';
 
 @Component({
   selector: 'app-footer',
@@ -7,4 +10,20 @@ import { Component } from '@angular/core';
 })
 export class FooterComponent {
 
+  cartProducts: { [id: number]: number } = {};
+  cartSubscription: Subscription | undefined;
+  cartItems: Product[] | undefined;
+  count:any = 0;
+
+  constructor( private cartService: CartService ){}
+
+  ngOnInit() {
+    this.cartSubscription = this.cartService.cartItems$.subscribe(items => {
+      this.cartItems = items;
+      console.log(this.cartItems,'footer')
+      this.count = this.cartItems.reduce((a,b)=>{
+        return a+=b.quantity;
+      },0)
+    })
+  }
 }
